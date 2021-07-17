92edec6239a2b26db284474c070b22a6c811beb8
Kyle <xKairu@users.noreply.github.com>
2021-07-17 22:40:59 +1200

    Update loona-vlive-badges.user.js

--- loona-vlive-badges.user.js
+++ loona-vlive-badges.user.js
@@ -9,19 +9,19 @@
 
 (function () {
   let userIds = [
-    { name: 'Heejin',     color: 'magenta',                 id: 'c9be75d0-9a25-11e8-9227-a0086f45fe1c' },
-    { name: 'Hyunjin',    color: 'yellow; color: black',    id: 'e645a450-0b7e-11ea-a87e-246e96398d40' },
-    { name: 'Haseul',     color: 'green',                   id: '70b62940-496d-11e9-99d3-a0086f9996be' },
-    { name: 'Yeojin',     color: 'orange; color: black',    id: 'b3f2d5a0-bd8b-11e8-a118-246e9648766c' },
-    { name: 'Vivi',       color: 'pink; color: black',      id: 'e49f2280-1eaa-11eb-8fcf-a0086f45fe06' },
-    { name: 'Kim Lip',    color: 'red',                     id: '476f21f0-d952-11e6-9fae-000000007398' },
-    { name: 'Jinsoul',    color: 'blue',                    id: '42698900-9b6b-11ea-9eaa-246e96487f70' },
-    { name: 'Choerry',    color: 'purple',                  id: '-missing-' },
-    { name: 'Yves',       color: 'brown',                   id: '76d03b80-8bf7-11e6-88df-000000002102' },
-    { name: 'Chuu',       color: 'peachpuff; color: black', id: '1fc49510-18cd-11e9-9777-246e96487868' },
-    { name: 'Go Won',     color: 'palegreen; color: black', id: 'c7fd2860-3b44-11e5-be21-000000001a8c' },
-    { name: 'Olivia Hye', color: 'grey; color: black',      id: '610b1690-de62-11eb-8ebb-246e9648766c' },
-    { name: 'soulbriz',   color: 'gold; color: black',      id: 'b32ae600-d25e-11e5-8239-000000003984' },
+    { name: 'Heejin',     color: 'magenta',                                           id: 'c9be75d0-9a25-11e8-9227-a0086f45fe1c' },
+    { name: 'Hyunjin',    color: 'yellow; color: black',                              id: 'e645a450-0b7e-11ea-a87e-246e96398d40' },
+    { name: 'Haseul',     color: 'green',                                             id: '70b62940-496d-11e9-99d3-a0086f9996be' },
+    { name: 'Yeojin',     color: 'orange; color: black',                              id: 'b3f2d5a0-bd8b-11e8-a118-246e9648766c' },
+    { name: 'Vivi',       color: 'pink; color: black',                                id: 'e49f2280-1eaa-11eb-8fcf-a0086f45fe06' },
+    { name: 'Kim Lip',    color: 'red',                                               id: '476f21f0-d952-11e6-9fae-000000007398' },
+    { name: 'Jinsoul',    color: 'blue',                                              id: '42698900-9b6b-11ea-9eaa-246e96487f70' },
+    { name: 'Choerry',    color: 'purple',                                            id: '-missing-' },
+    { name: 'Yves',       color: 'brown',                                             id: '76d03b80-8bf7-11e6-88df-000000002102' },
+    { name: 'Chuu',       color: 'peachpuff; color: black',                           id: '1fc49510-18cd-11e9-9777-246e96487868' },
+    { name: 'Go Won',     color: 'palegreen; color: black; border: 1px solid black;', id: 'c7fd2860-3b44-11e5-be21-000000001a8c' },
+    { name: 'Olivia Hye', color: 'grey; color: black',                                id: '610b1690-de62-11eb-8ebb-246e9648766c' },
+    { name: 'soulbriz',   color: 'gold; color: black',                                id: 'b32ae600-d25e-11e5-8239-000000003984' },
   ];
   
   // watch chatbox for updates
@@ -97,7 +97,7 @@
     const channelName = document.getElementsByClassName('channel_name--1VIVt')[0];
     if (channelName.getAttribute('title') !== '이달의 소녀(LOONA)') {
       console.log('Not on LOONA vlive, disabling.');
-      //return;
+      return;
     }
 
     let chatbox = document.getElementsByClassName('u_cbox_list')[0];
